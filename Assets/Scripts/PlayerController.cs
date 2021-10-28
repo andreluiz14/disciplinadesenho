6a410a28d8ed5c578d244cc3b7fb473065eb64d0
André Luiz Rodrigues de Oliveira <87727993+andreluiz14@users.noreply.github.com>
2021-10-28 10:48:25 -0700

    Correção personagem e adição da armadilha

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D player;
+    private float horizontal;
+    public float speed = 10;
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movemente();
+    }
+    private void Movemente()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        player.AddForce(Vector2.up * Time.deltaTime * speed);
+    }
+}
