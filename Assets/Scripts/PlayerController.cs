371a927109a65fc3a135d16470bd594f82288924
André Luiz Rodrigues de Oliveira <87727993+andreluiz14@users.noreply.github.com>
2021-10-29 18:19:58 -0400

    Atualização do personagem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Rigidbody2D player;
+    private Rigidbody2D rB;
     private float horizontal;
     public float speed = 10;
+    public float jump = 10;
+    private Vector2 move;
     void Start()
     {
-        player = GetComponent<Rigidbody2D>();
+        rB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Pulo();
         Movemente();
+
     }
+    private void FixedUpdate()
+    {
+        rB.AddForce(move * speed * Time.deltaTime, ForceMode2D.Force);
+    }
+
     private void Movemente()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        player.AddForce(Vector2.up * Time.deltaTime * speed * horizontal);
+        move = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+       
+    }
+    private void Pulo()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rB.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+        }
     }
 }
